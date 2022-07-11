b7769f4d29296a1d07f7440c7638302b312f57d8
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-07-11 11:51:28 -0400

    :art: Added listed product and post (single) structure
    Display all products on home page with the custom post type "producto"

--- index.php
+++ index.php
@@ -1,131 +1,51 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) { ?>
-  <?php while(have_posts()){the_post();?>
-    <?php the_content(); ?>  <!--Wordpress Content-->
-  <?php }?>
+  <?php while (have_posts()) {
+    the_post(); ?>
+    <?php the_content(); ?>
+    <!--Wordpress Content-->
+  <?php } ?>
 <?php } ?>
+
+<?php
+$plz_args = array(
+  // 'post_type' => 'producto', // Trae solo un producto, nombre de producto registrardo
+  'post_type' => array('producto'),
+  'posts_per_page' => -1, // numeros infinitos
+
+
+  // NOTA IMPORTANTE Reiniciar configuración de enlaces permanentes al crear custom post types para refrescar para generar configuración en archivo . htaccess
+);
+
+// Instanciar
+$productos = new WP_Query($plz_args);
+?>
 <div class="productos__container">
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a class="producto__link" href="#">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
+
+  <?php if ($productos->have_posts()) { ?>
+    <?php while ($productos->have_posts()) { $productos->the_post(); ?>
+      <div class="productos__card">
+        <figure>
+          <?php 
+          the_post_thumbnail("post-thumbnail");
+          // Posibles values 
+          // the_post_thumbnail("medium"); 
+          // the_post_thumbnail("large"); 
+          // the_post_thumbnail("ful"); 
+          ?>
+        </figure>
+        <div class="producto__caption">
+          <div class="productos__desc">
+            <a class="producto__link" href="<?php the_permalink(); // Cuando tiene el prefijo "the" imprime directamente y "get" solo lo va a traer?>">
+              <h4 class="productos__titulo"><?php the_title(); ?></h4>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a class="producto__link" href="#">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a class="producto__link" href="#">
-          <h4 class="productos__titulo">Round Shelf</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a href="">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a class="producto__link" href="#">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a href="">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a class="producto__link" href="#">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a href="">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_1.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a class="producto__link" href="#">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="productos__card">
-    <figure>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prod_2.png" alt="producto 1">
-    </figure>
-    <div class="producto__caption">
-      <div class="productos__desc">
-        <a href="">
-          <h4 class="productos__titulo">Retro refrigerato</h4>
-        </a>
-      </div>
-    </div>
-  </div>
+    <?php } ?>
+  <?php } ?>
+
 </div>
 
 
